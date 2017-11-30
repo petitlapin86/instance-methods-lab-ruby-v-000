e9599cf6f1ceb20d87e944d07059e1d4881314cc
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 16:58:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
-class Dog # I am creating a class 
+class Dog # I am creating a class
 
-def bark #I am defining an instance 
+def bark #I am defining an instance
   puts "Woof!"
 end
 
-end # I am ending the class 
+end # I am ending the class
